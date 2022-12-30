d31b25b3535e866526bf1ab6c6478f1d21786105
duy1510163nuce <duy1510163@nuce.edu.vn>
2022-12-30 13:27:09 +0700

    fix component no need to break down

--- src/app/components/modules/HomeContainer.tsx
+++ src/app/components/modules/HomeContainer.tsx
@@ -1,28 +1,76 @@
 import { FC, ReactElement } from "react";
-import Banner from "app/components/modules/Banner";
 import Achievement from "app/components/modules/Achievement";
-import Consultation from "app/components/modules/Consultation";
 import Footer from "app/components/modules/Footer";
 import Header from "app/components/modules/Header";
 import ProjectList from "app/components/modules/Projects";
 import Reputation from "app/components/modules/Reputation";
 import Service from "app/components/modules/ServiceList";
 import Submit from "app/components/modules/Submit";
-import User from "app/components/modules/User";
-import { listServices,listReason,listAchievement,listProjects,listNavbar } from "app/const/mockApi";
+import Button from "app/components/elements/Button";
+import Hero from "public/images/Hero.png";
+import avtUser from "public/images/user.png";
+import ConsulImg from "public/images/consultation.png";
+import "app/styles/modules/homeContainer.scss";
+import {
+  listServices,
+  listReason,
+  listAchievement,
+  listProjects,
+  listNavbar,
+} from "app/const/mockApi";
 
 export const HomeContainer: FC = (): ReactElement => {
   return (
     <div className="homeContainer">
-      <Header  items={listNavbar}/>
-      <Banner />
-      <Reputation reasons = {listReason} />
-      <User />
-      <Service services = {listServices} />
-      <Achievement achievements = {listAchievement} />
-      <Consultation/>
-      <ProjectList projects = {listProjects} />
-      <Submit/>
+      <Header items={listNavbar} />
+
+      <div className="banner">
+        <img alt="logo-box" src={Hero} className="banner-img" />
+      </div>
+      <Reputation reasons={listReason} />
+
+      <div className="user">
+        <img alt="avt-user" className="avt-user" src={avtUser} />
+        <div className="use-description">
+          <h1 className="des-title">About us</h1>
+          <p className="des-paragraph1">
+            For more than 30 years we have been delivering world-class
+            construction and we’ve built many lasting relationships along the
+            way.
+          </p>
+          <p className="des-paragraph2">
+            We’ve matured into an industry leader and trusted resource for those
+            seeking quality, innovation and reliability when building in the
+            U.S.
+          </p>
+          <Button className="des-btn">More on Our History</Button>
+        </div>
+      </div>
+
+      <Service services={listServices} />
+
+      <Achievement achievements={listAchievement} />
+
+      <div className="consultation">
+        <img
+          alt="img-consultation"
+          className="consultation-img"
+          src={ConsulImg}
+        />
+        <div className="consultation-left">
+          <h1 className="consultation-title">
+            Free consultation with exceptional quality
+          </h1>
+          <p className="consultation-text">
+            Just one call away:
+            <span className="consultation-phone"> +84 1102 2703</span>
+          </p>
+        </div>
+        <Button className="consultation-btn">Get your consultation</Button>
+      </div>
+
+      <ProjectList projects={listProjects} />
+      <Submit />
       <Footer />
     </div>
   );
